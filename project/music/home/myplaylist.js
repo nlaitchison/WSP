0aa6e9b8b552a17f0eae94d6dcb80e6d6160b27e
Nicole Aitchison <nlaitchison@gmail.com>
2013-03-29 02:47:10 -0400

    added music player to home page

---
+++ project/music/home/myplaylist.js
@@ -0,0 +1,42 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'music/home/10_This_Machine_Is_4',
+        oga:'music/home/10_This_Machine_Is_4.ogg',
+        artist:'NOFX',
+        title:'This Machine is 4',
+        album: 'Self Entitled',
+        duration:'0:30',
+        cover:'music/home/cover/nofx.png',
+
+    },
+
+    {
+        mp3:'music/home/10_Done_and_Dusted.mp3',
+        oga:'music/home/10_Done_and_Dusted.ogg',
+        artist:'Less Than Jake',
+        title:'Done and Dusted',
+        duration:'0:30',
+        cover:'music/home/cover/less_than_jake.png',
+
+    },
+
+    {
+        mp3:'music/home/The_Flatliners_-_I_Am_Abandoned.mp3',
+        oga:'music/home/The_Flatliners_-_I_Am_Abandoned.ogg',
+        artist:'The Flatliners',
+        title:'I Am Abandoned',
+        duration:'0:30',
+        cover:'music/home/cover/the_flatliners.png',
+
+    }
+
+];
